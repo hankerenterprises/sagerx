33577c08b33006a008bb02d7680e0185cd78e7ef
ndhammer <ndlyga@apphammer.co>
2024-08-17 08:27:42 -0400

    fixed subprocess command

--- airflow/dags/rxnorm/dag.py
+++ airflow/dags/rxnorm/dag.py
@@ -76,8 +76,10 @@ def rxnorm():
     # Task to transform data using dbt
     @task
     def transform():
-        run_subprocess_command(['docker', 'exec', 'dbt', 'dbt', 'run', '--select', 'models/staging/rxnorm', 'models/intermediate/rxnorm'], cwd='/dbt/sagerx')
-
+        # WHY???? not there is a docker in docker depenedency
+        # run_subprocess_command(['docker', 'exec', 'dbt', 'dbt', 'run', '--select', 'models/staging/rxnorm', 'models/intermediate/rxnorm'], cwd='/dbt/sagerx')
+        run_subprocess_command(['dbt', 'run', '--select', 'models/staging/rxnorm', 'models/intermediate/rxnorm'], cwd='/dbt/sagerx')
+        
     extract(get_st(get_tgt())) >> load >> transform()
 
 rxnorm()
